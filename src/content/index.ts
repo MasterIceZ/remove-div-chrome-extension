ad836c22bce0a4ddd0816747c88be1bb41fa84d4
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2023-07-07 00:28:26 +0700

    chore: remove เกียน

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,10 +1,7 @@
-console.info('chrome-ext template-vanilla-ts content script')
-console.info('เกียน')
 const elements = document.querySelectorAll('div.loading-spinner')
 
 console.log(elements)
 for (const element of elements) {
-  // element.appendChild(document.createTextNode('Hello from content script'))
   element.remove()
 }
 
